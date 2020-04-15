07c9635a7674527d2487a3951ebca2f582d763f1
Christopher Persons <christopher.persons@rallyhealth.com>
2020-04-15 09:07:00 -0700

    resolver and fetcher

--- src/main/scala/com/howtographql/scala/sangria/DAO.scala
+++ src/main/scala/com/howtographql/scala/sangria/DAO.scala
@@ -12,10 +12,6 @@ class DAO(db: Database) {
 
   def allLinks = db.run(Links.result)
 
-  def getLink(id: Int): Future[Option[Link]] = db.run(
-    Links.filter(_.id === id).result.headOption
-  )
-
   def getLinks(ids: Seq[Int]) = db.run(
     Links.filter(_.id inSet ids).result
   )
--- src/main/scala/com/howtographql/scala/sangria/GraphQLSchema.scala
+++ src/main/scala/com/howtographql/scala/sangria/GraphQLSchema.scala
@@ -2,23 +2,24 @@ package com.howtographql.scala.sangria
 
 import sangria.schema.{Field, ListType, ObjectType}
 import models._
-// #
+import sangria.execution.deferred.HasId
 import sangria.schema._
 import sangria.macros.derive._
-
+import sangria.execution.deferred.Fetcher
+import sangria.execution.deferred.DeferredResolver
 
 object GraphQLSchema {
+
   // 1 These are fields we want to expose in the DB
-  val LinkType = ObjectType[Unit, Link](
-    "Link",
-    fields[Unit, Link](
-      Field("id", IntType, resolve = _.value.id),
-      Field("url", StringType, resolve = _.value.url),
-      Field("description", StringType, resolve = _.value.description)
-    )
+  implicit val LinkType = deriveObjectType[Unit, Link]()
+  // to extract ids from entities
+  implicit val linkHasId = HasId[Link, Int](_.id)
+
+  val linksFetcher = Fetcher(
+    (ctx: MyContext, ids: Seq[Int]) => ctx.dao.getLinks(ids)
   )
-  // sangria.macros.derive._
-  // implicit val LinkType = deriveObjectType[Unit, Link]() //  macro way of defining LinkType
+
+  val Resolver = DeferredResolver.fetchers(linksFetcher)
 
   val Id = Argument("id", IntType)
   val Ids = Argument("ids", ListInputType(IntType))
@@ -29,11 +30,17 @@ object GraphQLSchema {
     fields[MyContext, Unit](
       Field("allLinks", ListType(LinkType), resolve = c => c.ctx.dao.allLinks),
       Field(
-        "link", OptionType(LinkType), arguments = Id :: Nil,
-        resolve = c => c.ctx.dao.getLink(c.arg[Int]("id"))),
+        "link",
+        OptionType(LinkType),
+        arguments = Id :: Nil,
+        resolve = c => linksFetcher.deferOpt(c.arg(Id))
+      ),
       Field(
-        "links", ListType(LinkType), arguments = Ids :: Nil,
-        resolve = c => c.ctx.dao.getLinks(c.arg[Seq[Int]]("ids")))
+        "links",
+        ListType(LinkType),
+        arguments = List(Argument("ids", ListInputType(IntType))),
+        resolve = c => linksFetcher.deferSeq(c.arg(Ids))
+      )
     )
   )
 
